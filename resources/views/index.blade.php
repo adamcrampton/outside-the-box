788427f68686333e64d075dbb95dd361a80205ba
Adam Crampton <adam@adamcrampton.com>
2021-12-02 11:22:14 +1100

    Initial setup

---
+++ resources/views/index.blade.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <meta name="description" content="">
+        <meta name="author" content="Adam Crampton">
+        <title>Think Outside The Box</title>
+        <link href="/css/vendor/bootstrap.min.css" rel="stylesheet">
+        <link href="/css/app.css" rel="stylesheet">
+    </head>
+    <body class="text-center">
+        <div class="cover-container d-flex w-100 h-100 p-3 mx-auto flex-column">
+            {{-- Header --}}
+            <header class="masthead mb-auto">
+                <div class="inner">
+                    <h3 class="masthead-brand">Think Outside The Box</h3>
+                    <nav class="nav nav-masthead justify-content-center">
+                       
+                    </nav>
+                </div>
+            </header>
+            {{-- Main --}}
+            <main role="main" class="inner cover">
+                <h1 class="cover-heading">H1.</h1>
+                <p class="lead">Lead</p>
+            </main>
+            {{-- Footer --}}
+            <footer class="mastfoot mt-auto">
+                <div class="inner">
+                    <p>Footer</p>
+                </div>
+            </footer>
+        </div>
+    </body>
+</html>
